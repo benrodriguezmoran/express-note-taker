9f0115f7bf4b8bcafeca9b8bad9c0083b50aef34
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-01-13 16:21:50 -0500

    server js

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+const PORT = 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
