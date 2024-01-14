4bf1cc78e887c6420e43285936b8fa448417e3e4
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-01-14 15:26:08 -0500

    server js note addition

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,44 @@
+const PORT = process.env.PORT || 3001; // Define the port for the server, using 3001.
+const path = require('path'); // Import the 'path' module to work with file paths.
+const fs = require('fs'); // Import the 'fs' module for file system operations.
 const express = require('express');
-const path = require('path');
-const api = require('./routes/index.js');
-const PORT = 3001;
-const app = express();
+const app = express(); // Create an instance of the Express application.
+const db = require('./db/db.json'); // Import notes data from 'db.json'.
 
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use(express.json());
+app.use(express.static('public')); 
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html')); // HTML for root route
+});
+app.get('/api/notes', (req, res) => {
+    res.json(db.slice(1)); // GET /api/notes, ignore first bugged entry.
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html')); // HTML /notes route
+});
 
+// Create a note
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+// POST Note
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, db);
+    res.json(newNote); // Respond to POST /api/notes with new note as JSON
+});
