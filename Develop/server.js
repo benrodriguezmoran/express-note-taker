409579c5424d11e64a5e6991fbcb7e4517fc0cc7
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-01-15 20:17:07 -0500

    possible fix for deploy

--- Develop/server.js
+++ Develop/server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html')); // HTML for root route
 });
 app.get('/api/notes', (req, res) => {
-    res.json(db); // GET /api/notes
+    res.sendFile(path.join(__dirname, dbPath)); // GET /api/notes
 });
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html')); // HTML /notes route
